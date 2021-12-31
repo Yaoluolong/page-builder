c042294be82e92390b1cc276fee13271054f183a
小难受 <yao_luolong@163.com>
2021-12-31 17:35:46 +0800

    babel配置

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          edge: '17',
-          firefox: '60',
-          chrome: '67',
-          safari: '11.1'
-        },
-        useBuiltIns: 'usage',
-        corejs: '3.6.4'
-      }
-    ]
-  ],
+  presets: [['@vue/cli-plugin-babel/preset']],
   plugins: ['@vue/babel-plugin-jsx']
 }
